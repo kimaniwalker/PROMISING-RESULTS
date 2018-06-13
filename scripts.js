d391e0dee1070724e180beafa466ab281594d2bf
Kimani Walker <kimaniwalker@gmail.com>
2018-06-13 13:20:52 -0500

    Finished lab

--- scripts.js
+++ scripts.js
@@ -9,4 +9,29 @@ slowMath.add(6,2)
 }).then((result) => {
     
     console.log(result);
+
+    return slowMath.divide(result,4)
+}).then((result) => {
+    console.log(result);
+    return slowMath.subtract(result,3)
+}).then((result) => {
+    console.log(result);
+    return slowMath.add(result,98)
+}).then((result) => {
+    console.log(result);
+    return slowMath.remainder(result,2)
+}).then((result) => {
+    console.log(result);
+    return slowMath.multiply(result,50)
+}).then((result) => {
+    console.log(result);
+    return slowMath.remainder(result,40)
+}).then((result) => {
+    console.log(result);
+    return slowMath.add(result,32)
+}).then((result) => {
+    console.log('The final result is' + result);
+
+}).catch((error) => {
+    console.log(error);
 })
